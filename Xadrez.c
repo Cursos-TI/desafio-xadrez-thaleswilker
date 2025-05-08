acc51929b2703bf870a4cbd900faa29f9655314a
thaleswilker <wilkerartthales2005@gmail.com>
2025-05-08 17:47:10 +0000

    Xadrez em C nivel novato

---
+++ Xadrez.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    printf("Movimento da Torre:\n");
+    for (int i = 1; i <= 5; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    printf("Movimento do Bispo:\n");
+    int j = 1;
+    while (j <= 5) {
+        printf("Cima Direita\n");
+        j++;
+    }
+
+    printf("\n");
+
+    printf("Movimento da Rainha:\n");
+    int k = 1;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k <= 8);
+
+    return 0;
+}
